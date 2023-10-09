0983767cadae0be9e08ff65a826ebaefc3bab798
Xanmoy <xanmoy@outlook.com>
2023-10-10 03:51:59 +0530

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,26 @@
+const projectsData = [
+  {
+    title: 'A Music App',
+    description: `Harmonica is your go-to music app for an immersive and personalized listening experience. Discover and enjoy your favorite songs, artists, and playlists from a vast library of music. With Harmonica, you can stream, create playlists, and explore new tunes effortlessly, all in one place. Elevate your music journey with Harmonica today!`,
+    imgSrc: '/static/images/harmonica.jpeg',
+    href: 'https://Music.xanmoy.me',
+  },
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. XanSearch has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://search.xanmoy.me',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
